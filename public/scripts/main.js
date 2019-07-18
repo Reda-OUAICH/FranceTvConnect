60714e805688c85bd61989f674ecad5d5fed84e4
blyndusk <alexandredelaloydev@gmail.com>
2019-07-19 01:20:54 +0200

    test color

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,23 @@
  const welcomePage = document.querySelector("#welcomePage");
  const playerStat = document.querySelector("#playerStat");
+ const body = document.querySelector('body')
 
  // register assistant canvas callbacks
  const callbacks = {
     onUpdate(state) {
         console.log('onUpdate', JSON.stringify(state));
-        if ('LebronJames' in state) playerStat.src = "../../public/assets/imgs/lebron-stats.jpg";
-        if ('DurantKevin' in state) playerStat.src = "../../public/assets/imgs/durant-stats.jpg"
-        if ('HardenJames' in state) playerStat.src = "../../public/assets/imgs/harden-stats.jpg"
+        if ('LebronJames' in state) {
+            playerStat.src = "../../public/assets/imgs/lebron-stats.jpg";
+            body.style.background = "red"
+        }
+        if ('DurantKevin' in state) {
+            playerStat.src = "../../public/assets/imgs/durant-stats.jpg";
+            body.style.background = "green"
+        }
+        if ('HardenJames' in state) {
+            playerStat.src = "../../public/assets/imgs/harden-stats.jpg";
+            body.style.background = "blue"
+        }
     },
  };
  assistantCanvas.ready(callbacks);
